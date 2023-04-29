a36b720044e8d048aaf33115e711732a5e3b0d9f
Girisha Sahdev <104763901+girishatechie@users.noreply.github.com>
2023-04-29 21:22:31 +0000

    Changing Name, Title and Social Media Profiles

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Girisha Sahdev",
+  title: "B.Tech Undergrad, IGDTUW",
+  email: "girishasahdev18@gmail.com",
+  gitHub: "girishatechie",
+  instagram: "girishasahdev",
+  linkedIn: "GirishaSahdev",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "GirishaSahdev",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
